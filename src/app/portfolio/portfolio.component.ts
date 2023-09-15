42ed0f7579a4179aad725a48ced1cb9e721c062f
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2023-09-15 21:20:37 +0200

    portfolio techno filter code edit

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -16,6 +16,13 @@ export class PortfolioComponent implements OnInit {
   projects: Project[]=Projects;
   selectedTechno="all";
 
+  Technos:Techno[]=[
+                    {name:"all",cssStatut:"filter-active"},
+                    {name:"angular",cssStatut:""},
+                    {name:"c#",cssStatut:""},
+                    {name:"java",cssStatut:""}
+                  ]
+
   allFilter:string="filter-active";
   angularFilter:string="";
   csharpFilter:string="";
@@ -32,49 +39,17 @@ export class PortfolioComponent implements OnInit {
     
   }
 
- 
-
   filter(techno:string):void{
     this.selectedTechno=techno;
-    if(techno=="all"){
-      this.activeAll();
-    }
-    if(techno=="angular"){
-      this.activeAngular();
-    }
-    if(techno=="c#"){
-      this.activeCsharp();
-    }
-    if(techno=="java"){
-      this.activeJava();
+    
+    for(var i=0;i<this.Technos.length; i++){
+      if(this.Technos[i].name!=techno){
+        this.Technos[i].cssStatut="";
+      }else{
+        this.Technos[i].cssStatut="filter-active";
+      }
     }
   }
 
-  //utiliser un tableau clé/valeur pour un code moins redondant
  
-  activeAll():void{
-    this.angularFilter=""
-    this.csharpFilter=""
-    this.javaFilter=""
-    this.allFilter="filter-active"
-  }
-  activeAngular():void{
-    this.angularFilter="filter-active"
-    this.csharpFilter=""
-    this.javaFilter=""
-    this.allFilter=""
-  }
-  activeCsharp():void{
-    this.angularFilter=""
-    this.csharpFilter="filter-active"
-    this.javaFilter=""
-    this.allFilter=""
-  }
-  activeJava():void{
-    this.angularFilter=""
-    this.csharpFilter=""
-    this.javaFilter="filter-active"
-    this.allFilter=""
-  }
-
 }
